342bcd90cd5fa1003dc6ecddd63ce447c42f7900
jerion <harry26934719@gmail.com>
2019-01-01 23:04:52 +0800

    card test

--- FP/src/card/Card.java
+++ FP/src/card/Card.java
@@ -1,17 +1,41 @@
 package card;
 
 public class Card {
-	public String Name;
-	public String Element;
-	public int Level;
-	public int Mana;
+	public String name;
+	public String element;
+	public int level;
+	public int mana;
+	public int attack;
+	public int add_mana;
+	public int add_card;
 	
-	public Card(String name, String element, int level, int mana)
+	public Card()
 	{
-		Name = name;
-		Element = element;
-		Level = level;
-		Mana = mana;
+		name = null;
+		element = null;
+		level = 0;
+		mana = 0;
+		attack = 0;
+		add_mana = 0;
+		add_card = 0;
+	}
+	
+	public void set_AttackCard(String Name, String Element, int Level, int Mana, int Attack)
+	{
+		name = Name;
+		element = Element;
+		level = Level;
+		mana = Mana;
+		attack = Attack;
 	}
 
+	public void set_EffectCard(String Name, String Element, int Level, int Mana, int AddMana, int AddCard)
+	{
+		name = Name;
+		element = Element;
+		level = Level;
+		mana = Mana;
+		add_mana = AddMana;
+		add_card = AddCard;
+	}
 }
--- FP/src/test.java
+++ FP/src/test.java
@@ -5,16 +5,8 @@ import card.Effect_Card;
 public class test {
 	public static void main (String[] args)
 	{
-		Atk_Card card1 = new Atk_Card(null, null, 0, 0, 0);
-		Effect_Card card2 = new Effect_Card(null, null, 0, 0, 0, 0);
-		Card[] cardlist = {
-				card1,
-				card2
-		};
-		
-		System.out.println(cardlist[0]);
-		System.out.println(card1);
-		
-		int a = cardlist[0].
+		Card cardlist[] = new Card[2];
+		cardlist[0].set_AttackCard(Name, Element, Level, Mana, Attack);
+		cardlist[1].set_EffectCard(Name, Element, Level, Mana, AddMana, AddCard);
 	}
 }
